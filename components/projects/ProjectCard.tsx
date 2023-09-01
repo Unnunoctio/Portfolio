96bf7ef297ab0fd50b47dcd3290cfaa541f10c56
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2023-09-01 01:43:08 -0400

    style: :lipstick: added min-h in project card title

--- components/projects/ProjectCard.tsx
+++ components/projects/ProjectCard.tsx
@@ -26,7 +26,7 @@ export const ProjectCard: React.FC<ProjectCardProps> = ({ order, id, title, webs
       <div className='relative overflow-hidden flex flex-col w-full h-[250px] bg-bg-project rounded-lg outline outline-offset-4 outline-4 outline-transparent transition-all duration-300 group-hover:animate-card-pulse group-hover:outline-card-outline'>
         <Image src={image} alt={title} width={450} height={280} className='object-cover w-full h-full' />
       </div>
-      <div className='flex items-center gap-2 px-1 mt-3'>
+      <div className='flex items-center gap-2 px-1 mt-3 min-h-[36px]'>
         <ProjectLogo logoId={id} style='fill-text-main' />
         <h3 className='text-2xl font-medium'>{title}</h3>
       </div>
