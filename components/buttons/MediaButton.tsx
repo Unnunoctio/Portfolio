06e81d2bf70f17a58c46d3dc0d5ec7bdc24f5353
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2023-09-02 18:22:17 -0400

    fix: :bug: fix target media button

--- components/buttons/MediaButton.tsx
+++ components/buttons/MediaButton.tsx
@@ -9,7 +9,7 @@ export const MediaButton: React.FC<MediaButtonProps> = ({ href, style, children 
   return (
     <a
       href={href}
-      target='_black'
+      target='_blank'
       rel='noreferrer'
       className={`px-4 py-2.5 text-lg font-medium text-text-secondary bg-button-bg rounded-2xl shadow-btn transition-colors hover:text-text-hover hover:bg-button-hover ${style as string}`}
     >
