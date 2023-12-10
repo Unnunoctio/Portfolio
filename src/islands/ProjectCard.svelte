0b236295ef31317d425037b978b6f2cc71aebeff
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2023-12-10 04:53:34 -0300

    perf: :zap: changed ProjectCard & ProjectLogo to svelte

--- src/islands/ProjectCard.svelte
+++ src/islands/ProjectCard.svelte
@@ -1,22 +1,23 @@
----
-import { Image } from 'astro:assets';
-import ProjectLogo from './ProjectLogo.astro';
-interface Props extends ProjectPreview {}
-
-const { id, title, preview, isReady } = Astro.props
----
+<script>
+  import ProjectLogo from "./ProjectLogo.svelte";
+  
+  export let id;
+  export let title;
+  export let preview;
+  export let isReady;
+</script>
 
 <a href={`/${id}`} data-astro-prefetch class="card group relative w-full max-w-[400px] cursor-pointer">
   <div class="absolute inset-0 w-full h-[270px] rounded-full transition-all duration-300 group-hover:shadow-card group-hover:animate-card-pulse group-hover:animate-card-shadow-pulse" />
   <div class="relative overflow-hidden w-full h-[270px] bg-bg-secondary rounded-lg outline outline-offset-4 outline-4 outline-transparent transition-all duration-300 group-hover:animate-card-pulse group-hover:outline-hover-outline">
-    { preview && <Image src={preview} alt={title} width={450} height={280} class={`object-cover w-full h-full ${isReady === false ? 'brightness-75 group-hover:brightness-100' : ''}`} /> }
-    { preview === undefined && isReady === true && <Image src="/projects/no_image.png" alt="no image project" width={450} height={280} class="object-cover w-full h-full" /> }
-    { preview === undefined && isReady === false && <Image src="/projects/developing.png" alt="developing project" width={450} height={280} class="object-cover w-full h-full brightness-75 group-hover:brightness-100" /> }
-    { isReady === false &&
+    {#if preview} <img src={preview} alt={title} width={450} height={280} class={`object-cover w-full h-full ${isReady === false ? 'brightness-75 group-hover:brightness-100' : ''}`} /> {/if}
+    {#if !preview && isReady} <img src="/projects/no_image.png" alt="no project" width={450} height={280} class="object-cover w-full h-full" /> {/if}
+    {#if !preview && !isReady} <img src="/projects/developing.png" alt="developing project" width={450} height={280} class="object-cover w-full h-full brightness-75 group-hover:brightness-100" /> {/if}
+    {#if !isReady}
       <span class="absolute bottom-0 w-full text-xl text-text-negative px-4 py-3 bg-gradient-to-r from-bg-active to-bg-active-light group-hover:invisible">
         En Desarrollo
       </span>
-    }
+    {/if}
   </div>
   <div class="flex items-center gap-2 px-1 mt-3 min-h-[36px]">
     <ProjectLogo id={id} style="fill-text-primary stroke-text-primary" />
