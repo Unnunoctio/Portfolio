1c1dc5b4184d81fc444c54536b0f7e76aecd68cd
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2023-12-13 03:05:32 -0300

    feat: :sparkles: added vercel ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import svelte from '@astrojs/svelte';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
   prefetch: true,
-  output: "hybrid"
+  output: "server",
+  adapter: vercel()
 });
