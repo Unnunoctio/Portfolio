d04b212e4ca0c9ee4a1fdc52f419a5fbd46b35bd
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2024-03-03 01:42:10 -0300

    refactor: :art: deleted extra comments and added nick in project path params

--- src/islands/project/ProjectList.svelte
+++ src/islands/project/ProjectList.svelte
@@ -3,10 +3,6 @@
   import ProjectCard from "./ProjectCard.svelte";
   import ProjectSkeleton from "./ProjectSkeleton.svelte";
   import { getProjects } from "../../api/getProjects";
-
-  // Get repositories in the store (save in cache)
-  // If not repositories => getProjects()
-  // Else view repositiories in the store
 </script>
 
 <ul class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8 mt-16">
