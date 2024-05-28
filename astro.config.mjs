bccc9c674cc1f36a977973e296c29d679fdf0977
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2024-05-28 14:34:41 -0400

    feat: :heavy_plus_sign: added dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
+import alpinejs from "@astrojs/alpinejs";
+import db from "@astrojs/db";
 import vercel from "@astrojs/vercel/serverless";
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), db(), alpinejs()],
   output: "server",
   adapter: vercel()
 });
